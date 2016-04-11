371518fbea0fe98485077325f2e3e6519bcf8d75
wornming <wornming@gmail.com>
2016-04-11 10:48:55 +0800

    添加对ViewPager的事件监听

--- src/com/zx/city/CityContentAdapter.java
+++ src/com/zx/city/CityContentAdapter.java
@@ -2,15 +2,16 @@ package com.zx.city;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import android.content.Context;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.ImageView.ScaleType;
 
 public class CityContentAdapter extends BaseAdapter {
@@ -24,6 +25,14 @@ public class CityContentAdapter extends BaseAdapter {
 	private List<ImageView> imageViews;
 	// 用于存放图片的ID
 	private int[] imageResId;
+	// 设置当前图片索引
+	private int currentItem = 0;
+	// 图片标题正文的那些点
+	private List<View> dots;
+	// viewpager下面的标题
+	private TextView adtitle;
+	// 图片标题
+	private String[] titles;
 
 	public CityContentAdapter(Context context) {
 		mContext = context;
@@ -33,18 +42,16 @@ public class CityContentAdapter extends BaseAdapter {
 	@Override
 	public int getCount() {
 		// 用于测试只显示5行数据
-		return 5;
+		return 30;
 	}
 
 	@Override
 	public Object getItem(int position) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public long getItemId(int position) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
@@ -53,7 +60,7 @@ public class CityContentAdapter extends BaseAdapter {
 		if (position == 0) {
 			return getTopView();
 		}
-		return null;
+		return getSecondView();
 	}
 
 	// ListView中实现PageAdapter
@@ -80,21 +87,65 @@ public class CityContentAdapter extends BaseAdapter {
 		}
 	}
 
+	// 得到第一个布局
 	private View getTopView() {
-
+		mTopView = LayoutInflater.from(mContext).inflate(R.layout.city_contentlist_viewpager, null);
 		imageResId = new int[] { R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d, R.drawable.e };
+
+		titles = new String[imageResId.length];
+		titles[0] = "我市将建全省首家大数据区域中心";
+		titles[1] = "扑树又回来啦！再唱经典老歌引万人大合唱";
+		titles[2] = "揭秘北京电影如何升级";
+		titles[3] = "乐视网TV版大派送";
+		titles[4] = "热血屌丝的反杀";
+
 		imageViews = new ArrayList<ImageView>();
 		// 初始化ImageView资源
-
 		for (int i = 0; i < imageResId.length; i++) {
 			ImageView imageView = new ImageView(mContext);
 			imageView.setImageResource(imageResId[i]);
 			imageView.setScaleType(ScaleType.CENTER_CROP);
 			imageViews.add(imageView);
 		}
-		mTopView = LayoutInflater.from(mContext).inflate(R.layout.city_contentlist_viewpager, null);
+
+		dots = new ArrayList<View>();
+		dots.add(mTopView.findViewById(R.id.v_dot0));
+		dots.add(mTopView.findViewById(R.id.v_dot1));
+		dots.add(mTopView.findViewById(R.id.v_dot2));
+		dots.add(mTopView.findViewById(R.id.v_dot3));
+		dots.add(mTopView.findViewById(R.id.v_dot4));
+
+		adtitle = (TextView) mTopView.findViewById(R.id.city_content_newstitle);
+		adtitle.setText(titles[0]);
+
 		viewPager = (ViewPager) mTopView.findViewById(R.id.view_pager);
 		viewPager.setAdapter(mPagerAdapter);
-		return viewPager;
+		viewPager.addOnPageChangeListener(new OnPageChangeListener() {
+			int oldPosition = 0;
+
+			@Override
+			public void onPageSelected(int position) {
+				currentItem = position;
+				adtitle.setText(titles[position]);
+				dots.get(oldPosition).setBackgroundResource(R.drawable.dot_normal);
+				dots.get(position).setBackgroundResource(R.drawable.dot_focused);
+				oldPosition = position;
+			}
+
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+			}
+		});
+		return mTopView;
+	}
+
+	// 得到第二个布局view
+	private View getSecondView() {
+
+		return LayoutInflater.from(mContext).inflate(R.layout.list_item, null);
 	}
 }
